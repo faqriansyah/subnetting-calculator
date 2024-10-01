9c14a3e2891bbc0ee15bf50d96e7d56ffcf480c2
faqriansyah <ifaqriansyah@gmail.com>
2024-10-01 09:03:22 +0800

    first commit

---
+++ js/subnetting.js
@@ -0,0 +1,9 @@
+const specialNumber = [128, 64, 32, 16, 8, 4, 2 ,1];
+
+function decimalToBinary(dec) {
+    const bin = (dec >>> 0).toString(2);
+}
+function binaryToDecimal(bin) {}
+function networkAddress(ip) {}
+function broadcastAddress(ip) {}
+function hostAddress(ip) {}
