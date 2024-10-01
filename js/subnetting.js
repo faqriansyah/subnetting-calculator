1ee0d5e7109193613029efc5a3a9ae47e631f2d5
faqriansyah <ifaqriansyah@gmail.com>
2024-10-01 19:04:53 +0800

    completed decimal to binary function

--- js/subnetting.js
+++ js/subnetting.js
@@ -1,27 +1,30 @@
-const specialNumber = [128, 64, 32, 16, 8, 4, 2 ,1];
 
-function binaryPad(bin) {
-    const diff = 8 - bin.length;
-    console.log(diff);
-    const str = "0";
-    let i = 0;
-    while (i < diff.length) {
-        str += "0";
-        i++;
-    }
-    return str + bin;
-}
+function realBinary() {}
+/**
+ * - Check if the digit is less than 255 if true :continue, if false : return 0 ✅
+ * - Convert dec to binary digit ✅
+ * - Check if the binary length is less than 8, if true : continue, if false : return the binary ✅
+ * - Make variable to calculate the diff between the binary digit length with 8  (n)  ✅
+ * - Add n 0 to the digit using loop ✅
+ */
 function decimalToBinary(dec) {
-    const bin = (dec >>> 0).toString(2);
-    if (bin.length < 8) {
-        return binaryPad(bin);
+    if (dec <= 255) {
+        let bin = dec.toString(2);
+        if (bin.length < 8) {
+            const diff = 8 - bin.length;
+            for (let i = 0; i < diff; i++) bin = '0' + bin ;
+            return bin;
+        } else {
+            return bin;
+        }
+    } else {
+        return 0;
     }
-    return bin;
 }
-function binaryToDecimal(bin) {}
-function networkAddress(ip) {}
-function broadcastAddress(ip) {}
-function hostAddress(ip) {}
+function binaryToDecimal(bin) { 
 
+}
+function networkAddress(ip) { }
+function broadcastAddress(ip) { }
+function hostAddress(ip) { }
 
-console.log(decimalToBinary(10));
