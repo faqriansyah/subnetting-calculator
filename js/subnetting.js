4e0fc9edcb0f775f8d05846d93dc9bbaed7c63c0
faqriansyah <ifaqriansyah@gmail.com>
2024-10-05 22:55:51 +0800

    filter done

--- js/subnetting.js
+++ js/subnetting.js
@@ -1,7 +1,27 @@
 const specialNumber = [128, 64, 32, 16, 8, 4, 2, 1];
 
+
+// Filter 
+const check_decimal = (p) => {
+    if(p > 255) return false;
+    if(p < 0) return false;
+    return true;
+}
+
+const check_binary = (p) => {
+    if(p.length < 8 || p.length > 8) return false;
+    if(!/^[01]+$/.test(p)) return false;
+    return true;
+}
+
+const filter = (p, base) => {
+    if(base == 2) return check_binary(p);
+    if(base == 8) return check_decimal(p);
+}
+
+// Conversion
 const convert_decimal_to_binary = (dec) => {
-    if(filter(dec, 8)) return
+    if(!filter(dec, 8)) return null;
     let binary = "";
     for (let i = 0; i < specialNumber.length; i++) {
         binary += dec >= specialNumber[i] ? "1" : "0";
@@ -12,6 +32,7 @@ const convert_decimal_to_binary = (dec) => {
 
 
 const convert_binary_to_decimal = (bin) => {
+    if(!filter(bin, 2)) return null;
     let decimal = 0;
     for (let i = 0; i < specialNumber.length; i++) {
         decimal += bin[i] == "0" ? 0 : specialNumber[i]; 
