5901021e095031dc38b117b48ae4c96d5b63a0d1
faqriansyah <ifaqriansyah@gmail.com>
2024-10-05 08:43:41 +0800

    update convertion method

--- js/subnetting.js
+++ js/subnetting.js
@@ -1,30 +1,39 @@
+const specialNumber = [128, 64, 32, 16, 8, 4, 2, 1];
 
-function realBinary() {}
-/**
- * - Check if the digit is less than 255 if true :continue, if false : return 0 ✅
- * - Convert dec to binary digit ✅
- * - Check if the binary length is less than 8, if true : continue, if false : return the binary ✅
- * - Make variable to calculate the diff between the binary digit length with 8  (n)  ✅
- * - Add n 0 to the digit using loop ✅
- */
-function decimalToBinary(dec) {
-    if (dec <= 255) {
-        let bin = dec.toString(2);
-        if (bin.length < 8) {
-            const diff = 8 - bin.length;
-            for (let i = 0; i < diff; i++) bin = '0' + bin ;
-            return bin;
-        } else {
-            return bin;
+const check_if_can_reduced = (dec, i) => {
+    const reduce = dec - i;
+    const check = Math.sign(reduce);
+    if(check == -1) return false;
+    if(check == 0) return true;
+    if(check == 1) return true; 
+}
+
+const convert_decimal_to_binary = (dec) => {
+    let binary = "";
+    for (let i = 0; i < specialNumber.length; i++) {
+        if(check_if_can_reduced(dec, specialNumber[i])) {
+            dec -= specialNumber[i];
+            binary += "1";
+        }  else {
+            binary += "0";
         }
-    } else {
-        return 0;
     }
+    return binary;
 }
-function binaryToDecimal(bin) { 
 
+const convert_binary_to_decimal = (bin) => {
+    let decimal = 0;
+    for (let i = 0; i < specialNumber.length; i++) {
+        if(bin[i] == "0") decimal += 0;
+        if (bin[i] == "1") decimal += specialNumber[i];
+    }
+    return decimal;
 }
-function networkAddress(ip) { }
-function broadcastAddress(ip) { }
-function hostAddress(ip) { }
 
+const convert = (p, base) => {
+    if(base == 8) return convert_decimal_to_binary(p);
+    if(base == 2) return convert_binary_to_decimal(p);
+}
+
+console.log(convert(255, 2));
+// console.log(Math.sign(0));
